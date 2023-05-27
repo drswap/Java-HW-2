1ea1db90760927ed8005380ddda357485596375b
Swap <swap15.2014@gmail.com>
2023-05-27 12:10:06 +0300

    src/

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+
+    public static void main(String[] args){
+        int ticketAmount,Bonus;
+        int each_Bonus = 20;
+        ticketAmount= 19555;
+        Bonus= ticketAmount/each_Bonus;
+
+        System.out.println("Задание 1");
+        System.out.println("Стоимость билета =" + ticketAmount);
+        System.out.println ("Мили за перелёты =" + Bonus);
+
+
+    }
+
+}
